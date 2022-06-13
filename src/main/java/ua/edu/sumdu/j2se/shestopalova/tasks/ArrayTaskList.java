abfc5e88b319efc5ddbe9225f4ed841296ef8fac
Ksenia <strangeksu@gmail.com>
2022-06-13 20:53:07 +0300

    Lab_6 Iterator

--- src/main/java/ua/edu/sumdu/j2se/shestopalova/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/shestopalova/tasks/ArrayTaskList.java
@@ -1,12 +1,15 @@
 package ua.edu.sumdu.j2se.shestopalova.tasks;
 
-public class ArrayTaskList extends AbstractTaskList {
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+public class ArrayTaskList extends AbstractTaskList  {
 
     private Task[] taskArray = new Task[10];
 
     private int size = 0;
 
-
+@Override
    public void add(Task task) throws IllegalArgumentException{
 
         if (size >= taskArray.length) {
@@ -19,7 +22,7 @@ public class ArrayTaskList extends AbstractTaskList {
 
        if(task == null) throw new IllegalArgumentException("Task can't be null");
     }
-
+@Override
     public boolean remove(Task task) {
 
         if (task != null) {
@@ -33,23 +36,20 @@ public class ArrayTaskList extends AbstractTaskList {
         }
         return false;
    }
-
+    @Override
     public int size(){
         return size;
     }
-
-   public Task getTask(int index) throws IndexOutOfBoundsException{
-       if ((index >= size) && (index < 0)) {
-           throw new IndexOutOfBoundsException("There is no task with such index");
-       }
-       return taskArray[index];
-
+    @Override
+    public Task getTask(int index) throws IndexOutOfBoundsException {
+        if(index < 0 || index >= size) throw new IndexOutOfBoundsException("Index is out of range for the list");
+        return taskArray[index];
     }
 
     public ListTypes.types getType() {
         return ListTypes.types.ARRAY;
     }
-
+ @Override
    public ArrayTaskList incoming(int from, int to){
        ArrayTaskList incomTasks = new ArrayTaskList();
 
@@ -65,5 +65,44 @@ public class ArrayTaskList extends AbstractTaskList {
        }
        return incomTasks;
    }
+
+    @Override
+    public Iterator<Task> iterator() {
+        return new Iterator<Task>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public Task next() {
+                return taskArray[index++];
+            }
+
+            @Override
+            public void remove() {
+                if(index == 0 ) throw new IllegalStateException();
+                ArrayTaskList.this.remove(taskArray[--index]);
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        Iterator<Task> taskIterator = this.iterator();
+        StringBuilder string = new StringBuilder("ArrayTaskList{");
+        while (taskIterator.hasNext()) {
+            string.append(taskIterator.next().toString()).append(", ");
+        }
+        return string.append("size=").append(size).append('}').toString();
+    }
+
+    public Stream<Task> getStream(){
+        return Stream.of(this.taskArray);
+    }
+
+
 
 }
